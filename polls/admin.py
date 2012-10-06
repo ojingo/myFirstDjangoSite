fa8eaf611a5f33cd5260452cf7238592fc6bc774
TJ <tj@ojingolabs.com>
2012-10-05 22:11:06 -0400

    editing poll model and choices for giving many choices key

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,18 @@
 from polls.models import Poll
 from django.contrib import admin
+from polls.models import Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Poll, PollAdmin)
