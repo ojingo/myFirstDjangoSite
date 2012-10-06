88b515cf08dc27153be63a5b022ee642df490da0
TJ <tj@ojingolabs.com>
2012-10-06 18:26:09 -0400

    simple refactor of detail with shortcut

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from polls.models import Poll
 from django.http import HttpResponse
-from django.http import Http404
 
 
 def index(request):
@@ -10,10 +9,7 @@ def index(request):
 
 
 def detail(request, poll_id):
-    try:
-        p = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+    p = get_object_or_404(Poll, pk=poll_id)
     return render_to_response('polls/detail.html', {'poll': p})
 
 
