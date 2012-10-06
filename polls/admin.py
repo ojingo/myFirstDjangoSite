f6ee8f9e9141d5a1b05784dbbbd54c1b9e41414e
TJ <tj@ojingolabs.com>
2012-10-06 00:08:12 -0400

    inline tabular!

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from polls.models import Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
