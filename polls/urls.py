5844a6993770907bf2f472bb2a055294f91896f1
TJ <tj@ojingolabs.com>
2012-10-06 18:40:51 -0400

    tweak to fix but has a bug include not used?

--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('polls.views',
-    url(r'^polls/$', 'index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
-)
-
-urlpatterns += patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
