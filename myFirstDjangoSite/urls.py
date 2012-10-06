efed2429432f96dfd6e449c46a3bc6c8f3acb0da
TJ <tj@ojingolabs.com>
2012-10-06 18:35:37 -0400

    tweak urls patterns to remove repetitive parts

--- myFirstDjangoSite/urls.py
+++ myFirstDjangoSite/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+urlpatterns = patterns('polls.views',
+    url(r'^polls/$', 'index'),
+    url(r'^polls/(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
+)
+
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
