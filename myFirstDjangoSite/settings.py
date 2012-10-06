17d6597762b25123659f8758d97563e2747bb7bd
TJ <tj@ojingolabs.com>
2012-10-06 00:24:23 -0400

    brought in my own templates by cloning the built in templates and over-riding them

--- myFirstDjangoSite/settings.py
+++ myFirstDjangoSite/settings.py
@@ -103,6 +103,7 @@ ROOT_URLCONF = 'myFirstDjangoSite.urls'
 WSGI_APPLICATION = 'myFirstDjangoSite.wsgi.application'
 
 TEMPLATE_DIRS = (
+    '/Users/tmacbook/Documents/CODING/django/Sites/myFirstDjangoSite/myFirstDjangoSite/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
