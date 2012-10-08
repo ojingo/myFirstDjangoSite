e3d0e291da8d19f944d3038febdcfdecd7592119
TJ <tj@ojingolabs.com>
2012-10-07 21:50:51 -0400

    tweak for serving files from foreman???

--- myFirstDjangoSite/urls.py
+++ myFirstDjangoSite/urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include, url, settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,3 +9,11 @@ urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+            }),
+    )
