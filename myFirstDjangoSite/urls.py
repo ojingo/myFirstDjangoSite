fe0845ddc327e247ae5d759f68a5e66d51be2704
TJ <tj@ojingolabs.com>
2012-10-10 23:06:33 -0400

    $ added to end of hello/

--- myFirstDjangoSite/urls.py
+++ myFirstDjangoSite/urls.py
@@ -10,7 +10,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hello/', hello),
+    url(r'^hello/$', hello),
 )
 
 if settings.DEBUG:
