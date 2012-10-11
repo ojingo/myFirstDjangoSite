133f88882f368744951b1326ea2db3ee01a74359
TJ <tj@ojingolabs.com>
2012-10-11 00:24:07 -0400

    fix back to work - error examined

--- myFirstDjangoSite/views.py
+++ myFirstDjangoSite/views.py
@@ -17,10 +17,10 @@ def current_datetime(request):
 
 
 def hours_ahead(request, offset):
-    #try:
-    #    offset = int(offset)
-    #except ValueError:
-    #    raise Http404()
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
     html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
     return HttpResponse(html)
